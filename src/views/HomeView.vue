d91c7221b4311ba745b8ab2ec074906233aac407
Akshat Gupta <akshatg1274@hotmail.com>
2022-06-07 18:10:34 +0100

    MAP-52: Adding landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,56 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue';
+console.log('In home');
 </script>
-
 <template>
-    <main>
-        <HelloWorld msg="You did it!!" />
-    </main>
+    <header>
+        <nav>
+            <h1 id="logo">MAPHIS</h1>
+            <ul id="navigation-container">
+                <li><a href="">Home</a></li>
+                <li><a href="">Mission</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
+        <div id="background">
+            <img src="../images/Example_FM.png" alt="" />
+            <img src="../images/Map_BG_2.png" alt="" />
+        </div>
+    </header>
+    <main></main>
+    <footer></footer>
 </template>
+<style>
+nav {
+    display: flex;
+    justify-content: space-between;
+}
+#logo {
+    flex-grow: 1;
+}
+#navigation-container {
+    display: flex;
+    flex-grow: 2;
+    justify-content: space-between;
+    align-items: center;
+}
+#navigation-container li {
+    list-style: none;
+}
+#navigation-container a {
+    text-decoration: none;
+}
+div#background {
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-between; */
+    height: 100vh;
+    z-index: 0;
+    transform: rotate(-40deg);
+    position: relative;
+}
+div#background img:nth-child(2) {
+    margin-top: 30px;
+}
+</style>
