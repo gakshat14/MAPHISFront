3065217c94c7198bfe634dd9607084b88c760592
Akshat Gupta <akshatg1274@hotmail.com>
2022-07-14 12:41:19 +0100

    Adding map tiles and temp geojson file for testing

--- src/components/LeafLet.vue
+++ src/components/LeafLet.vue
@@ -1,19 +1,55 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { map, tileLayer, type TileLayerOptions } from 'leaflet';
+import { onMounted, ref } from 'vue';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import {
+    map,
+    tileLayer,
+    type TileLayerOptions,
+    CRS,
+    RasterCoords,
+    geoJSON,
+    type PointExpression,
+    type Content,
+} from 'leaflet';
 import { uri } from '@/utils/networkUtils';
+import { tile_data_text } from '../geojson/text';
+import '../../node_modules/leaflet-rastercoords/rastercoords.js';
 
 const region = 'york';
+const foo = ref<HTMLElement>();
 
 onMounted(() => {
-    const leafMap = map('leaflet-container').setView([0, 0], 0);
+    const img = [4096, 4096];
+    const leafMap = map('leaflet-container', { crs: CRS.Simple });
+    const rc = new RasterCoords(leafMap, img);
+    leafMap.setView(rc.unproject([4096, 4096]), 2);
 
     const options: TileLayerOptions = {
+        minZoom: 0,
         maxZoom: 4,
         noWrap: true,
+        bounds: rc.getMaxBounds(),
+        maxNativeZoom: rc.zoomLevel(),
     };
 
     tileLayer(`${uri}map-tiles/${region}/{z}/{x}/{y}.jpg`, options).addTo(leafMap);
+
+    const textLayer = geoJSON(tile_data_text.features, {
+        coordsToLatLng: function (coords) {
+            return rc.unproject(coords as PointExpression);
+        },
+        onEachFeature: function (feature, layer) {
+            if (feature?.properties?.class) {
+                layer.bindPopup(() => foo.value?.innerHTML as Content);
+            }
+        },
+        style: function (feature) {
+            return { fillColor: 'darkgray', color: '#d5d5d5', fillOpacity: 0.5 };
+        },
+    });
+
+    leafMap.addLayer(textLayer);
 });
 </script>
 
